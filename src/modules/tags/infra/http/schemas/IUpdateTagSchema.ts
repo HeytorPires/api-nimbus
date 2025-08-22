a70f995cb600b9beb4cdeabc66e3be0b5889fdee
HeytorPires <heytor.cacho29@gmail.com>
2025-08-22 11:17:43 -0300

    :bug: fix: schema

--- src/modules/tags/infra/http/schemas/IUpdateTagSchema.ts
+++ src/modules/tags/infra/http/schemas/IUpdateTagSchema.ts
@@ -1,12 +1,12 @@
 import * as yup from 'yup';
 
-const tagCreateSchema = yup.object({
-  // params: yup.object({
-  //   id: yup.string().uuid().required(),
-  // }),
+const tagUpdateSchema = yup.object({
+  params: yup.object({
+    id: yup.string().uuid().required(),
+  }),
   body: yup.object({
     name: yup.string().required(),
   }),
 });
 
-export { tagCreateSchema };
+export { tagUpdateSchema };
