a708153fd108e8e6f3934bac73505b63250c8790
HeytorPires <heytor.cacho29@gmail.com>
2026-06-17 11:06:39 -0300

    arrumando variaveis do docker

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,3 +1,21 @@
 import { createConnection } from 'typeorm';
 
-createConnection();
+createConnection({
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: Number(process.env.DB_PORT) || 5432,
+  username: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASSWORD || '123456',
+  database: process.env.DB_NAME || 'nimbus-api',
+  entities: [
+    process.env.NODE_ENV === 'production'
+      ? 'build/src/modules/**/infra/typeorm/entities/*.js'
+      : 'src/modules/**/infra/typeorm/entities/*.ts',
+  ],
+  migrations: [
+    process.env.NODE_ENV === 'production'
+      ? 'build/src/shared/infra/typeorm/migrations/*.js'
+      : 'src/shared/infra/typeorm/migrations/*.ts',
+  ],
+  migrationsRun: false,
+});
